dfb4b7abcaf9069743dff1c063db007da31acef7
Juliano <julianoferrarigianlupi@gmail.com>
2020-04-03 11:50:01 -0400

    added myself to authors

--- CC3D/Models/BiocIU/SARSCoV2MultiscaleVTM/Model/Simulation/CoronavirusSteppables.py
+++ CC3D/Models/BiocIU/SARSCoV2MultiscaleVTM/Model/Simulation/CoronavirusSteppables.py
@@ -1,7 +1,7 @@
 ###############################################################################################################
 # To cite this model please use the following:
 #
-# Josua Aponte-Serrano, T.J. Sego, James A. Glazier,
+# Josua Aponte-Serrano, T.J. Sego, Juliano F. Gianlupi, James A. Glazier,
 # "Model of Viral Tissue Infection"
 # https://github.com/covid-tissue-models/covid-tissue-response-models/tree/master/tellurium/simple_virus_model
 ###############################################################################################################
