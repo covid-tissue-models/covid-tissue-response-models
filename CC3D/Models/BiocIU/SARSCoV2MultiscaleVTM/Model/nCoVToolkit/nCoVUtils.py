88799f3719753b88ec408dda86f5c65df72d7eb8
tjsego <tjsego@gmail.com>
2020-04-19 18:39:27 -0400

    Utility to export parameters defined in a module to csv

--- CC3D/Models/BiocIU/SARSCoV2MultiscaleVTM/Model/nCoVToolkit/nCoVUtils.py
+++ CC3D/Models/BiocIU/SARSCoV2MultiscaleVTM/Model/nCoVToolkit/nCoVUtils.py
@@ -2,6 +2,25 @@
 # hosted by the Biocomplexity Institute at Indiana University
 
 
+def export_parameters(param_module, export_file):
+    """
+    Exports parameters defined in a module to csv
+    :param param_module: module containing parameters
+    :param export_file: location of file to write csv
+    :return: None
+    """
+    import csv
+
+    params_list = [x for x in dir(param_module) if not (x.startswith('__') and x.endswith('__'))]
+    params_dict = dict()
+    for k in params_list:
+        params_dict[k] = getattr(param_module, k)
+
+    with open(export_file, 'w', newline='') as fout:
+        csv_data_writer = csv.writer(fout, delimiter=',')
+        [csv_data_writer.writerow([k, v]) for k, v in params_dict.items()]
+
+
 def hill_equation(val, diss_cf, hill_cf):
     """
     Hill equation
