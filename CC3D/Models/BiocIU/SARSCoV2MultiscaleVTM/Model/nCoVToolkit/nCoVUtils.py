7be46a2abfbd9c67b7962ffce43cac84eb991672
tjsego <tjsego@gmail.com>
2020-04-22 18:50:57 -0400

    Added support for exporting csv of model parameters with a description

--- CC3D/Models/BiocIU/SARSCoV2MultiscaleVTM/Model/nCoVToolkit/nCoVUtils.py
+++ CC3D/Models/BiocIU/SARSCoV2MultiscaleVTM/Model/nCoVToolkit/nCoVUtils.py
@@ -11,18 +11,32 @@ def export_parameters(param_module, export_file):
     """
     import csv
 
+    param_desc = None
+    desc_key = '__param_desc__'
+
     if isinstance(param_module, dict):
         params_dict = param_module
+        if desc_key in params_dict.keys():
+            param_desc = params_dict.pop(desc_key)
     else:
         params_dict = dict()
         params_list = [x for x in dir(param_module) if not (x.startswith('__') and x.endswith('__'))]
         for k in params_list:
             params_dict[k] = getattr(param_module, k)
+        if desc_key in dir(param_module):
+            param_desc = getattr(param_module, desc_key)
 
     with open(export_file, 'w', newline='') as fout:
         csv_data_writer = csv.writer(fout, delimiter=',')
-        [csv_data_writer.writerow([k, v]) for k, v in params_dict.items()]
-
+        for k, v in params_dict.items():
+            if param_desc is not None:
+                this_desc = ' '
+                if k in param_desc.keys():
+                    this_desc = param_desc[k]
+                row_to_write = [k, v, this_desc]
+            else:
+                row_to_write = [k, v]
+            csv_data_writer.writerow(row_to_write)
 
 def hill_equation(val, diss_cf, hill_cf):
     """
