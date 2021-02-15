19b0d5b93338586e20b3727dd5aa623fb638bf7d
tjsego <tjsego@gmail.com>
2021-02-15 14:53:06 -0500

    Updated docs to reflect purpose of the overall framework

--- CC3D/Models/BiocIU/SARSCoV2MultiscaleVTM/Model/__init__.py
+++ CC3D/Models/BiocIU/SARSCoV2MultiscaleVTM/Model/__init__.py
@@ -4,11 +4,24 @@ Modular viral infection modeling and simulation framework project
 
 Hosted by the Biocomplexity Institute at Indiana University.
 
+Basic information
+=================
+
 For more information on the purpose and scope of the framework, see https://doi.org/10.1371/journal.pcbi.1008451.
 
 To download a free, local copy of CompuCell3D, visit https://compucell3d.org.
 
 To deploy this tool in your favorite browser, visit https://nanohub.org/tools/cc3dcovid19.
 
-For documentation of usage and development of the framework, see ``Models.readme.rst``.
+For documentation of usage and development of the framework, see ``Models.Readme.rst``.
+
+For tutorials on the basics of downloading and running the framework in CompuCell3D, see the directory ``Tutorials``.
+
+To cite this framework please use the following:
+
+T.J. Sego, Josua O. Aponte-Serrano, Juliano Ferrari Gianlupi, Samuel R. Heaps, Kira Breithaupt, Lutz Brusch,
+Jessica Crawshaw, James M. Osborne, Ellen M. Quardokus, Richard K. Plemper, James A. Glazier,
+"A modular framework for multiscale, multicellular, spatiotemporal modeling of acute primary viral infection and
+immune response in epithelial tissues and its application to drug therapy timing and effectiveness",
+PLoS Comput Biol 16(12): e1008451. https://doi.org/10.1371/journal.pcbi.1008451
 """
