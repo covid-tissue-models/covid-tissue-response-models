6e659e7b98a2ac91a63649ecdb38e155897e7b11
Juliano Ferrari Gianlupi <35964356+JulianoGianlupi@users.noreply.github.com>
2022-02-28 14:29:04 -0500

    lytic death inv dict

--- CC3D/Models/BiocIU/SARSCoV2MultiscaleVTM/Model/investigation_dictionaries.py
+++ CC3D/Models/BiocIU/SARSCoV2MultiscaleVTM/Model/investigation_dictionaries.py
@@ -4,6 +4,11 @@ Written by JFG
 
 """
 
+from numpy import arange, around
+
 diffusion_investigation_only_change = {'media_selection': list(range(4))}
 
-diffusion_investigation= {'media_selection': list(range(5))}
+diffusion_investigation = {'media_selection': list(range(5))}
+
+lytic_non_lytic_inv = {'lytic_prob_mult': list(around(arange(0, 1.1, 0.1),1))}
+
